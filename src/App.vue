fc2985e9a8614f4dc684de5aa25e374ed6df2f96
Alireza Far <imail01999@gmail.com>
2020-12-07 00:11:49 +0330

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile  from "./components/UserProfile";
+
+export default {
+  name: 'App',
+  components: {
+      UserProfile
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+button {
+    width: 8.33%;
+    border: 2px solid #41B883;
+    background-color: #fff;
+    padding: 8px 16px;
+    border-radius: 2px;
+    color: #41B883;
+    font-weight: bold;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: all .2s linear;
+    outline: none;
+}
+button:hover {
+    background-color: #41B883;
+    color: #fff;
+}
+button:active {
+    background-color: #318f65;
+    border-color: #318f65;
+}
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{ user.username }}</h1>
+            <div class="user-profile__follower-count">
+                <strong>Followers: </strong> {{ followers }}
+            </div>
+            <button class="btn btn-info mt-4" @click="followUser">Follow</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserProfile",
+    data() {
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+                username: "ar_ielz",
+                firstName: "Alireza",
+                lastName: "Far",
+                email: "aariow01@gmail.com",
+                isAdmin: true
+            }
+        }
+    },
+    watch: {
+        followers(newFollowerCount, oldFollowerCount) {
+            if (oldFollowerCount < newFollowerCount) {
+                console.log(`${this.user.username} has gained a follower`);
+            }
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.user.firstName} ${this.user.lastName}`;
+        }
+    },
+    methods: {
+        followUser() {
+            this.followers++;
+        }
+    },
+    mounted() {
+        console.warn("Component mounted");
+    },
+    unmounted() {
+        console.warn("Component unmounted");
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
