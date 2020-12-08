c2f4265c5ecea9d5a5bb678eba32a21855dc6b8e
Alireza Far <imail01999@gmail.com>
2020-12-09 01:28:48 +0330

    vue.config.js file has been added for configuring /src/styles/base.scss to be set for global stylign. It does not work yet!

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  background-color: #eceff3;
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
